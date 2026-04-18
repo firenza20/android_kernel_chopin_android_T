8a24af2e95a07e9ab4c19912a3a900fa5c813c8c
firenza <masudtxg@gmail.com>
2026-04-19 02:52:41 +0700

    feat: implement path_umount helper and add strncpy_from_user_nofault

--- mm/maccess.c
+++ mm/maccess.c
@@ -105,3 +105,28 @@ long strncpy_from_unsafe(char *dst, const void *unsafe_addr, long count)
 
 	return ret ? -EFAULT : src - unsafe_addr;
 }
+
+long strncpy_from_user_nofault(char *dst, const void __user *unsafe_addr, long count)
+{
+	mm_segment_t old_fs = get_fs();
+	long ret;
+
+	if (unlikely(count <= 0))
+		return 0;
+
+	set_fs(USER_DS);
+	pagefault_disable();
+	ret = strncpy_from_user(dst, unsafe_addr, count);
+	pagefault_enable();
+	set_fs(old_fs);
+
+	if (ret >= count) {
+		ret = count;
+		dst[ret - 1] = '\0';
+	} else if (ret > 0) {
+		ret++;
+	}
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(strncpy_from_user_nofault);
