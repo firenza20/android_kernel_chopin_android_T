7e79b0b07b8a33c0318df2daf31a987c019c05ec
firenza <masudtxg@gmail.com>
2026-05-06 23:26:29 +0700

    refactor: introduce no-fault string copy helpers, implement path_umount utility, and add filter_count to seccomp structure

--- include/linux/uaccess.h
+++ include/linux/uaccess.h
@@ -255,6 +255,8 @@ extern long notrace probe_kernel_write(void *dst, const void *src, size_t size);
 extern long notrace __probe_kernel_write(void *dst, const void *src, size_t size);
 
 extern long strncpy_from_unsafe(char *dst, const void *unsafe_addr, long count);
+extern long strncpy_from_kernel_nofault(char *dst, const void *unsafe_addr, long count);
+extern long strncpy_from_user_nofault(char *dst, const void __user *unsafe_addr, long count);
 
 /**
  * probe_kernel_address(): safely attempt to read from a location
